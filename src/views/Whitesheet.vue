c9a9e9fa0e00a71e1c219342cb78a2d6163738d8
Araam Zaremehrjardi <araamzaremehrjardi@gmail.com>
2022-12-28 03:52:53 -0800

    Adding Information to Pages

--- src/views/Whitesheet.vue
+++ src/views/Whitesheet.vue
@@ -1,16 +1,59 @@
 <script lang="ts">
+    import whitesheet_json from '../assets/information/whitesheet.json'
+
+    export default {
+        data() {
+            return {
+                whitesheet: whitesheet_json
+            }
+        }
+    }
 
 </script>
 
 <template>
     <div class="whitesheet">
-        <div>
+        <div class="section">
             <h3>
                 Biography
             </h3>
-            <p>
-                Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test
+            <p class="body-text">
+                {{  whitesheet.whitesheet.biography.body }}
             </p>
+        </div>
+        <div class="section">
+            <h3>
+                Education
+            </h3>
+            <div>
+                <div v-for="institution in whitesheet.whitesheet.education">
+                    <p>
+                        {{ institution.university }}
+                    </p>
+                    <p>
+                        {{  institution.degree }}
+                    </p>
+                    <p>
+                        GPA: {{  institution.gpa }}
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="section">
+            <h3>
+                Experience 
+            </h3>
+            <div>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="section">
+            <h3>
+                Skills
+            </h3>
+            <div></div>
         </div>
     </div>
 </template>
@@ -20,12 +63,21 @@
 @media only screen and (min-width: 300px) {
     .whitesheet {
         padding: 30px;
+        display: flex;
+        flex-direction: column;
+        gap: 30px;
+    }
+    .section {
+        display: flex;
+        gap: 6px;
+        flex-direction: column
+    }
+    .body-text {
     }
     p {
-        white-space: normal;
-        word-break: break-word;
-        overflow-wrap: break-word;
-        font-size: 0.6rem;
+        font-size: 0.7rem;
+        line-height: 1rem;
+        text-align: justify;
     }
 
     
