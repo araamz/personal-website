0be6f08f5987e6e49b8a642b7838d1e27c275c84
araamz <araamzaremehrjardi@gmail.com>
2025-12-01 19:12:21 -0800

    Revisting old website

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<article class="h-screen w-screen bg-linear-to-tr from-neutral-900 to-stone-900">
+	<section class="max-w-[500px] mx-auto px-10 pt-40">
+		<header class="flex flex-col gap-2">
+			<h1 class="text-8xl text-stone-600 font-semibold">
+                Hello.
+            </h1>
+            <p class="text-4xl text-stone-400 font-medium text-wrap">
+                Developer. Coffee Lover. Avid Learner.
+            </p>
+		</header>
+	</section>
+</article>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+	plugins: [tailwindcss(), sveltekit()]
+});
